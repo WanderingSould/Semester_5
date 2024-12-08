dd4d7ce4f76ed5a7e16d7538bc3843ab62a7beb5
Shenoy <shenoymaster23@gmail.com>
2024-12-08 15:04:30 +0530

    validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-function validation() {
-    let a =10
+
+let registration = [
+    // input inu id kodukana same names
+
+    "name" , "age" , "phone" , "male" , "female" , "address", 
+]
+
+
+function validate() {
+    let raidoChecked = true
+
+    
+    registration.forEach(element => {
+        let input = document.getElementById(element)
+        let value = input.value
+
+        // inputs validate cheyyan
+        if (value == "") {
+            alert(element + " not filled")
+        }
+
+        // radio button validate cheyyan
+        else if (element == "male" || element == "female") {
+            if (!input.checked) {
+                raidoChecked = false
+            }
+        }
+
+        
+        else if (element == "address") {
+            if (value.trim() == "") {
+                alert(element + " not filled")
+            }
+        }
+    });
+    if (!raidoChecked) {
+        alert("gender not filled")
+
+    }
 }
